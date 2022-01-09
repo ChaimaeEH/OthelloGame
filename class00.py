cbc165c467ed6483a036e7ba83e41c89031f6179
Chaimae EL HOUJJAJI <90857148+ChaimaeEH@users.noreply.github.com>
2022-01-10 00:36:36 +0100

    correction fautes orthographe + enlever import inutiles

--- class00.py
+++ class00.py
@@ -7,21 +7,14 @@ Created on 10/01/2022
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
-import random
-import time
-# import State as Othello
 from collections import defaultdict
 
 # -----------------------------
 # import fonctions hors class
 # -----------------------------
 
-from fonctions00 import drawBoard, resetBoard, isOnBoard, getBoardCopy, \
-    enterPlayerTile, getPlayerMove, \
-    getHintsBoard, showPoints, getScoreOfBoard, \
-    is_game_over, get_legal_actions, isValidMove, flipTiles, \
-    playAgain
+from fonctions00 import getBoardCopy, getScoreOfBoard, \
+    is_game_over, get_legal_actions, flipTiles
 
 
 class MonteCarloTreeSearchNode(object):
@@ -43,7 +36,7 @@ class MonteCarloTreeSearchNode(object):
     def untried_actions(self):
         """ 
         Returns:
-            list: Actions légales non encore essayées pour une configuation
+            list: Actions légales non encore essayées pour une configuration
                   de jeu donnée pour le joueur dont c'est le tour.
         """
         self._untried_actions = get_legal_actions(self.state, self.tile)
@@ -70,7 +63,7 @@ class MonteCarloTreeSearchNode(object):
     
 
     def expand(self):
-        """A cette étape, un nouveau noeud enfant possible est généré et est ajouté au tableau des enfants. 
+        """À cette étape, un nouveau noeud enfant possible est généré et est ajouté au tableau des enfants. 
             Il constitue un nouveau noeud du tree search. 
             Ce nouveau noeud est associé au tour de l'autre joueur.
 
@@ -93,7 +86,7 @@ class MonteCarloTreeSearchNode(object):
 
 
     def getChildState(self, action, tile):
-        """Change l'état du plateau de jeu selon l'action envisagée'.
+        """Change l'état du plateau de jeu selon l'action envisagée.
 
         Args:
             action (list): couple des coordonnées de la case à jouer.          
@@ -121,7 +114,7 @@ class MonteCarloTreeSearchNode(object):
 
     def rollout(self):
         """ À partir de l'état actuel, la partie est simulée jusqu'à la fin de partie. 
-            Tous les coups sont choisis de aléatoirement parmi l'ensemble des coups possibles, 
+            Tous les coups sont choisis aléatoirement parmi l'ensemble des coups possibles, 
             on parle de jeu léger.
 
         Returns:
@@ -136,10 +129,10 @@ class MonteCarloTreeSearchNode(object):
         while not is_game_over(current_rollout_state):                               # tant que la partie simulée n'est pas terminée
             possible_moves = get_legal_actions(current_rollout_state, current_tile)  # liste des coups possibles
             if possible_moves !=[] :                                                 # si au moins un coup est possible
-                action = possible_moves[np.random.randint(len(possible_moves))]      # tier au hasard un coup possible
+                action = possible_moves[np.random.randint(len(possible_moves))]      # tirer au hasard un coup possible
                 flipTiles(current_rollout_state, current_tile, action[0], action[1]) # effectuer ce coup aléatoire
             
-            # donner son tour à l'autre joueur dont le coup sera tiré au sort de la meme facon
+            # donner son tour à l'autre joueur dont le coup sera tiré au sort de la même façon
             if current_tile == 'X': 
                  current_tile = 'O'
             else:
@@ -153,7 +146,7 @@ class MonteCarloTreeSearchNode(object):
     def game_result(self, resultats):
         """
         Args:
-            resultats (int): [description]
+            resultats (int): résultat du jeu, c'est un dictionnaire avec le résultat de X et de O
 
         Returns:
             int: 1 ou 0 ou -1 selon que l'état correspondant à une victoire, une égalité ou une défaite.
@@ -173,7 +166,7 @@ class MonteCarloTreeSearchNode(object):
 
 
     def backpropagate(self, result):
-        """Mise à jour des statistiques de chaque nœuds de la branche contenant le noeud enfant considéré
+        """Mise à jour des statistiques de chaque nœud de la branche contenant le noeud enfant considéré
            i.e remontée dans l'arbre du noeud enfant au noeud racine (qui n'a pas de parent) 
            --> récursivité de backpropagate().
            
@@ -197,15 +190,15 @@ class MonteCarloTreeSearchNode(object):
             i.e toutes les actions possibles ont été utilisées pour générer un enfant.
 
         Returns:
-            bool: True si le neoud est complètement développé
+            bool: True si le noeud est complètement développé
                   False sinon.
         """
         return len(self._untried_actions) == 0
 
 
     def best_child(self, c_param):
-        """ Sélection du meilleur enfant (UCB le plus élevé) parmis tous les enfants possibles
-        du neoud considéré. 
+        """ Sélection du meilleur enfant (UCB le plus élevé) parmi tous les enfants possibles
+        du noeud considéré. 
         Le premier terme de la formule de l'UCB correspond à l'exploitation et le second terme à l'exploration.
 
         Args:
@@ -241,7 +234,7 @@ class MonteCarloTreeSearchNode(object):
     def best_action(self,simulation_no,c_param):
         """Permet de choisir la meilleure action i.e qui renvoie le nœud enfant correspondant au meilleur coup
         possible connu. 
-        Les étapes d'expansion/selection, de simulation et de rétropropagation sont réalisées par le code
+        Les étapes d'expansion/sélection, de simulation et de rétropropagation sont réalisées par le code
         ci-dessus autant de fois que la valeur du paramètre simulation_no.
 
         Args:
